a7265f98aa00d25fcbb21b815e3cba8887f3701a
chentao <chentao@blingstorm.com.cn>
2014-12-15 19:02:19 +0800

    todo小实例

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todoApp
+ */
+angular.module('todoApp')
+    .controller('MainCtrl', function($scope, localStorageService) {
+        var todosInStore = localStorageService.get('todos');
+
+        $scope.todos = todosInStore || [];
+
+        $scope.$watch('todos', function() {
+            localStorageService.set('todos', $scope.todos);
+        }, true);
+
+
+        $scope.addTodo = function() {
+            $scope.todos.push($scope.todo);
+            $scope.todo = '';
+        };
+        $scope.removeTodo = function(index) {
+            $scope.todos.splice(index, 1);
+        };
+    });
